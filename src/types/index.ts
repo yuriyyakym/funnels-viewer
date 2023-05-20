23a378fee7bceb023dbc82388982eb3d3a61e3bc
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-20 19:41:05 +0700

    Export types
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export { type default as Alignment } from './Alignment';
+export { type default as Block } from './Block';
+export { type default as ButtonBlock } from './ButtonBlock';
+export { type default as Color } from './Color';
+export { type default as Funnel } from './Funnel';
+export { type default as ListBlock } from './ListBlock';
+export { type default as ListItem } from './ListItem';
+export { type default as Page } from './Page';
+export { type default as TextBlock } from './TextBlock';
