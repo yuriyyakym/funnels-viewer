42b5c17cb22940e25669deb7165ccfa4dc77880c
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-20 18:38:16 +0700

    Add Block type
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

---
+++ src/types/Block.ts
@@ -0,0 +1,8 @@
+import ButtonBlock from './ButtonBlock';
+import ImageBlock from './ImageBlock';
+import ListBlock from './ListBlock';
+import TextBlock from './TextBlock';
+
+type Block = ButtonBlock | TextBlock | ListBlock | ImageBlock;
+
+export default Block;
