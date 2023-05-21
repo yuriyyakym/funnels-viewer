25fdcc05d39c756622aeebcc166bc57d6b1ba888
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-21 17:24:53 +0700

    Add isListBlock typeguard
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/ListBlock.ts
+++ src/types/ListBlock.ts
@@ -1,8 +1,16 @@
-import Id from './Id';
-import ListItem from './ListItem';
+import { isObject } from 'utils';
+
+import type Id from './Id';
+import { type default as ListItem, isListItem } from './ListItem';
 
 export default interface ListBlock {
   id: Id;
   type: 'list';
   items: ListItem[];
 }
+
+export const isListBlock = (value: unknown): value is ListBlock =>
+  isObject(value) &&
+  value.type === 'list' &&
+  Array.isArray(value.items) &&
+  value.items.every(isListItem);
