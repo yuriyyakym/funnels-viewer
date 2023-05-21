6b5fddd8e73d44ddbea12601ba70664720f66af2
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-21 17:24:38 +0700

    Add isListItem typeguard
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/ListItem.ts
+++ src/types/ListItem.ts
@@ -1,5 +1,13 @@
+import { isObject } from 'utils';
+
 export default interface ListItem {
   title: string;
   description: string;
   src: string;
 }
+
+export const isListItem = (value: unknown): value is ListItem =>
+  isObject(value) &&
+  typeof value.title === 'string' &&
+  typeof value.description === 'string' &&
+  typeof value.src === 'string';
