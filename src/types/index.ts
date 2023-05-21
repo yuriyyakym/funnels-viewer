72ffc920ab2c83a81fa1d39149ac7f526eb3aa65
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-21 19:45:30 +0700

    Export isListBlock and isTextBlock typeguards
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export { type default as ButtonBlock, isButtonBlock } from './ButtonBlock';
 export { type default as Color } from './Color';
 export { type default as Funnel } from './Funnel';
 export { type default as ImageBlock, isImageBlock } from './ImageBlock';
+export { type default as ListBlock, isListBlock } from './ListBlock';
 export { type default as ListItem } from './ListItem';
 export { type default as Page } from './Page';
-export { type default as TextBlock } from './TextBlock';
+export { type default as TextBlock, isTextBlock } from './TextBlock';
