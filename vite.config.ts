6d020621b3142eed61859e4e4e53e7a0fd6b5210
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-21 18:52:45 +0700

    Configure aliases
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      modules: path.resolve(__dirname, 'src/modules'),
+      state: path.resolve(__dirname, 'src/state'),
+      types: path.resolve(__dirname, 'src/types'),
+      utils: path.resolve(__dirname, 'src/utils'),
+    },
+  },
+});
