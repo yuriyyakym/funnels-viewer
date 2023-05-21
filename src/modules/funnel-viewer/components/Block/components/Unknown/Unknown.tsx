dff1f95a256879c2abe044264d90a0c7f93fec49
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-21 21:02:55 +0700

    Remove className prop from funnel Unknown component
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/modules/funnel-viewer/components/Block/components/Unknown/Unknown.tsx
+++ src/modules/funnel-viewer/components/Block/components/Unknown/Unknown.tsx
@@ -1,17 +1,23 @@
-import classNames from 'classnames';
 import { FunctionComponent } from 'react';
 
 import { Block } from 'types';
 
-interface Props extends Pick<Block, 'id' | 'type'> {
-  className?: string;
-}
+type Props = Partial<Pick<Block, 'id' | 'type'>>;
 
-const Unknown: FunctionComponent<Props> = ({ className, id, type }) => (
-  <div className={classNames(className, 'p-2 bg-yellow-200 text-slate-600')}>
-    <p>There is either no renderer for "{type}" block type or data format mismatch.</p>
-    <p>Please contact our support in order to get more details.</p>
-    <p>Block ID: {id}</p>
+const Unknown: FunctionComponent<Props> = ({ id, type }) => (
+  <div className="p-2 bg-yellow-200 text-slate-600">
+    {typeof type === 'undefined' && <p>Unknown block type.</p>}
+    {typeof type !== 'undefined' && (
+      <p>There is either no renderer for "{type}" block type or data format mismatch.</p>
+    )}
+
+    <p>Please contact our support to get more details.</p>
+
+    {typeof id === 'string' && (
+      <p>
+        Block ID: <code>{id}</code>
+      </p>
+    )}
   </div>
 );
 
