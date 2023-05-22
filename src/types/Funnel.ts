070842c6b9f0f4eb070a110d542c0f3a243da85e
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-23 01:34:06 +0700

    Add isFunnel typeguard
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/Funnel.ts
+++ src/types/Funnel.ts
@@ -1,8 +1,17 @@
+import { isObject } from 'utils';
+
 import Color from './Color';
-import Page from './Page';
+import Page, { isPage } from './Page';
 
 export default interface Funnel {
   name: string;
   bgColor: Color;
   pages: Page[];
 }
+
+export const isFunnel = (value: unknown): value is Funnel =>
+  isObject(value) &&
+  typeof value.name === 'string' &&
+  typeof value.bgColor === 'string' &&
+  Array.isArray(value.pages) &&
+  value.pages.every(isPage);
