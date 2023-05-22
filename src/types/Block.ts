b27524c9de03b388c62986fde72145ffec95bccc
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-23 01:33:49 +0700

    Add isBlock typeguard
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/Block.ts
+++ src/types/Block.ts
@@ -1,8 +1,11 @@
-import ButtonBlock from './ButtonBlock';
-import ImageBlock from './ImageBlock';
-import ListBlock from './ListBlock';
-import TextBlock from './TextBlock';
+import ButtonBlock, { isButtonBlock } from './ButtonBlock';
+import ImageBlock, { isImageBlock } from './ImageBlock';
+import ListBlock, { isListBlock } from './ListBlock';
+import TextBlock, { isTextBlock } from './TextBlock';
 
 type Block = ButtonBlock | TextBlock | ListBlock | ImageBlock;
+
+export const isBlock = (value: unknown): value is Block =>
+  [isButtonBlock, isTextBlock, isListBlock, isImageBlock].some((typeguard) => typeguard(value));
 
 export default Block;
