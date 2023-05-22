73ac7b2eba89a38fc288dab9ac772336aa735547
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-23 01:36:11 +0700

    Add isPage typeguard
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/types/Page.ts
+++ src/types/Page.ts
@@ -1,7 +1,15 @@
-import Block from './Block';
+import { isObject } from 'utils';
+
+import Block, { isBlock } from './Block';
 import Id from './Id';
 
 export default interface Page {
   id: Id;
   blocks: Block[];
 }
+
+export const isPage = (value: unknown): value is Page =>
+  isObject(value) &&
+  typeof value.id === 'string' &&
+  Array.isArray(value.blocks) &&
+  value.blocks.every(isBlock);
