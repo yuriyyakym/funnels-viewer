7f104852ecbb26f845ebb11d96725aa7aa3b1a01
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-25 02:22:39 +0700

    Remove main-panel module
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/modules/main-panel/components/ViewportSelect/types.ts
+++
@@ -1,6 +0,0 @@
-import { Size } from 'types';
-
-export interface ViewportSizeOption {
-  label: string;
-  value: Size;
-}
--- src/modules/main-panel/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PagesNavigation } from './PagesNavigation';
-export { default as ViewportSelect } from './ViewportSelect';
-export { default as ViewportSizeInput } from './ViewportSizeInput';
