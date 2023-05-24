cbe6539ce0bb764085e3f974144ea4ce43752cc4
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-24 11:48:17 +0700

    Update App component
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import classNames from 'classnames';
 import { FunctionComponent } from 'react';
 
-const App: FunctionComponent = () => <>Funnels Viewer</>;
+import FunnelViewer from 'modules/funnel-viewer';
+import MainPanel from 'modules/main-panel';
+import Header from 'modules/header';
+import { useActivePage, useFunnel } from 'state';
+
+const App: FunctionComponent = () => {
+  const funnel = useFunnel();
+  const page = useActivePage();
+
+  return (
+    <div className="flex w-full h-full overflow-hidden">
+      <aside
+        className={classNames(
+          'w-aside min-w-aside h-full p-2',
+          'border-r border-gray-200',
+          'bg-gray-50',
+          'overflow-auto',
+        )}
+      >
+        <Header />
+        <MainPanel className="mt-5" />
+      </aside>
+
+      <main className="flex-1 p-2 overflow-hidden">
+        <FunnelViewer backgroundColor={funnel?.bgColor} page={page} />
+      </main>
+    </div>
+  );
+};
 
 export default App;
