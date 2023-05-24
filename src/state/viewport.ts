39d8b27b933d90837f9677ca0f073fed62d9c6b1
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-25 02:28:05 +0700

    Refactor viewport state
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/state/viewport.ts
+++ src/state/viewport.ts
@@ -1,13 +1,11 @@
-import { useAtomValue, useSetAtom } from 'jotai';
+import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 import { Size } from 'types';
 
 const viewportSizeAtom = atomWithStorage<Size>('preview-viewport-size', {
-  width: 1440,
+  width: 466,
   height: 900,
 });
 
-export const useViewportSize = () => useAtomValue(viewportSizeAtom);
-
-export const useSetViewportSize = () => useSetAtom(viewportSizeAtom);
+export const useViewportSizeState = () => useAtom(viewportSizeAtom);
