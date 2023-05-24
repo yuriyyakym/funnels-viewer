b97ba6e11c5abc2e4a8b0a3bd190fcdf81f0784b
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-24 22:27:50 +0700

    Pass className to Message component
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import { FunctionComponent } from 'react';
 import classNames from 'classnames';
 
-type MessageProps = {
-  variant: 'info' | 'error' | 'warning';
+type Props = {
   children: string;
+  className?: string;
+  variant: 'info' | 'error' | 'warning';
 };
 
-const Message: React.FC<MessageProps> = ({ children, variant }) => (
+const Message: FunctionComponent<Props> = ({ children, className, variant }) => (
   <div
-    className={classNames('p-4 w-full', {
+    className={classNames(className, 'p-4 w-full', {
       'bg-blue-100 text-blue-700': variant === 'info',
       'bg-red-100 text-red-700': variant === 'error',
       'bg-yellow-100 text-yellow-700': variant === 'warning',
