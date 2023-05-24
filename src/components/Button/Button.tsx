5eb6c00bb07c5829aef83c32517a3adb128207eb
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-05-25 04:37:36 +0700

    Add Button variants
    Signed-off-by: Yuriy Yakym <yakym.yuriy@gmail.com>

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
-import { ButtonHTMLAttributes, FunctionComponent } from 'react';
 import classNames from 'classnames';
+import { ButtonHTMLAttributes, FunctionComponent } from 'react';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  variant?: 'blue' | 'slate' | 'yellow';
 }
 
-const Button: FunctionComponent<Props> = ({ className, children, ...rest }) => {
+const Button: FunctionComponent<Props> = ({ className, children, variant = 'blue', ...rest }) => {
   return (
     <button
       className={classNames(
@@ -13,7 +14,11 @@ const Button: FunctionComponent<Props> = ({ className, children, ...rest }) => {
         'py-1 px-2',
         'rounded',
         'text-white disabled:text-opacity-80 font-bold',
-        'bg-blue-400 hover:bg-blue-500',
+        {
+          'bg-blue-400 hover:bg-blue-500 disabled:bg-blue-500': variant === 'blue',
+          'bg-slate-400 hover:bg-slate-500 disabled:bg-slate-500': variant === 'slate',
+          'bg-yellow-400 hover:bg-yellow-500 disabled:bg-yellow-500': variant === 'yellow',
+        },
       )}
       {...rest}
     >
